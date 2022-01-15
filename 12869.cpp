22cfbce0619d7a0515b7cd5bc923ae2920012395
domicmeia <dhdlqks55@gmail.com>
2022-01-15 21:37:15 +0900

    12869

--- 12869.cpp
+++ 12869.cpp
@@ -4,28 +4,33 @@ using namespace std;
 
 int dp[61][61][61], a[3], n, visited[61][61][61];
 int _a[6][3] = {
-    {9, 3, 1},{9, 1, 3},{3, 1, 9},{3, 9, 1},{1, 3, 9},{1, 9, 3}
-};
+    {9, 3, 1}, {9, 1, 3}, {3, 1, 9}, {3, 9, 1}, {1, 3, 9}, {1, 9, 3}};
 
-struct A{
+struct A
+{
     int a, b, c;
 };
 
-queue<A>q;
-int solve{int a, int b, int c}{
-    visited[a][b][c] =1;
-    q.push({a,b,c});
-    while(q.size()){
+queue<A> q;
+int solve(int a, int b, int c)
+{
+    visited[a][b][c] = 1;
+    q.push({a, b, c});
+    while (q.size())
+    {
         int a = q.front().a;
         int b = q.front().b;
         int c = q.front().c;
         q.pop();
-        if(visited[0][0][0]) break;
-        for(int i=0;i<6;i++){
-            int nexta = max(0, a -_a[i][0]);
-            int nextb = max(0, b -_a[i][1]);
-            int nextc = max(0, c -_a[i][2]);
-            if(visited[nexta][nextb][nextc]) continue;
+        if (visited[0][0][0])
+            break;
+        for (int i = 0; i < 6; i++)
+        {
+            int nexta = max(0, a - _a[i][0]);
+            int nextb = max(0, b - _a[i][1]);
+            int nextc = max(0, c - _a[i][2]);
+            if (visited[nexta][nextb][nextc])
+                continue;
             visited[nexta][nextb][nextc] = visited[a][b][c] + 1;
             q.push({nexta, nextb, nextc});
         }
@@ -33,12 +38,14 @@ int solve{int a, int b, int c}{
     return visited[0][0][0] - 1;
 }
 
-int main(){
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     cin >> n;
-    for(int i=0; i<n; i++) cin >> a[i];
-    fout << solve(a[0], a[1], a[2]) << "\n";
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    cout << solve(a[0], a[1], a[2]) << "\n";
     return 0;
 }
